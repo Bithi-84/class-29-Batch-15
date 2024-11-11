0ac91fccd80e3f045be7872ac32e2fb993c46fdc
Bithi-84 <tehzeebbithi.2016@gmail.com>
2024-11-11 13:51:28 +0600

    class29

---
+++ script.js
@@ -0,0 +1,50 @@
+
+
+
+let indexArray = [43, 98, 34, 56,33, 65];
+
+   console.log(indexArray[4]);
+
+   console.log(indexArray[0]);
+
+   console.log(indexArray[5]);
+
+
+let student = {name: "Rahim", age: 12, blood: "o+"};
+
+console.log(student.age);
+
+console.log(student.name);
+
+let twoDimansionalArray = [
+
+    [43, 98, 32, 2],
+    [11, 81, 39, 47],
+    [12, 5, 100, 77],
+    [91, 1, 2, 0]
+];
+
+console.log(twoDimansionalArray[0][0]);
+console.log(twoDimansionalArray[0][1]);
+console.log(twoDimansionalArray[0][2]);
+console.log(twoDimansionalArray[0][3]);
+
+
+console.log(twoDimansionalArray[1][0]);
+console.log(twoDimansionalArray[1][1]);
+console.log(twoDimansionalArray[1][2]);
+console.log(twoDimansionalArray[1][3]);
+
+console.log(twoDimansionalArray[2][0]);
+console.log(twoDimansionalArray[2][1]);
+console.log(twoDimansionalArray[2][2]);
+console.log(twoDimansionalArray[2][3]);
+
+
+console.log(twoDimansionalArray[3][0]);
+console.log(twoDimansionalArray[3][1]);
+console.log(twoDimansionalArray[3][2]);
+console.log(twoDimansionalArray[3][3]);
+
+
+
